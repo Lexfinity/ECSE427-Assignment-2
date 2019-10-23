d2ded477d399587329f3f80b4a55c3dd59cbf4e4
Ammar Rudani <aarudani7@gmail.com>
2019-10-23 14:40:08 -0400

    Update assignment_2.c

--- assignment_2.c
+++ assignment_2.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <time.h>
 
 //declaring/initializing shared data
 int read_count = 0;
@@ -10,78 +11,135 @@ static sem_t mutex;
 static sem_t rw_mutex;
 // int inputData = 0;
 
+//declaring/initializing time variables
+double min_waitW = 1000000000000000;
+double max_waitW = -1;
+double min_waitR = 1000000000000000;
+double max_waitR = -1;
+double avg_waitW, avg_waitR;
+
+int SleepTime(){
+    return rand() % 100000;
+}
+
+struct timespec before, after;
+
 static void *reader(void *arg)
 {
 
-int total = *((int *)  arg);
-int i = 0;
-  while(i < total)
+  int total = *((int *)arg);
+  int i = 0;
+  while (i < total)
   {
-    //clock_t start_wait = clock(); 
+    float random = SleepTime(); 
+    clock_t start_time = clock();
+    // clock_gettime(CLOCK_MONOTONIC, &before);
     sem_wait(&mutex);
+
     read_count++;
     if (read_count == 1)
       sem_wait(&rw_mutex);
 
     sem_post(&mutex);
-    printf("reader\n");
+    clock_t wait = clock() -start_time;
+    // clock_gettime(CLOCK_MONOTONIC, &after);
+    double time = ((double)wait) / CLOCKS_PER_SEC;
+
+  // double seconds = (double) (after.tv_sec - before.tv_sec) * 1000000;
+  // double nanosec = (after.tv_nsec - before.tv_nsec) / 1000;
+  // double time = seconds + nanosec;
+
+    if (time < min_waitR)
+    {
+      min_waitR = time;
+    }
+
+    if (time > max_waitR)
+    {
+      max_waitR = time;
+    }
+
+    avg_waitR += time;
+
+    printf("reader ");
     sem_wait(&mutex);
     read_count--;
     if (read_count == 0)
       sem_post(&rw_mutex);
     sem_post(&mutex);
+    usleep(random);
     i++;
-  } //while (1);
+  }
 }
 
 static void *writer(void *arg)
 {
-int total = *((int *)  arg);
-int i = 0;
-  while(i < total)
+  int total = *((int *)arg);
+  int i = 0;
+  while (i < total)
   {
-    //clock_t start_wait = clock(); 
+    float random = SleepTime(); 
+    clock_t start_time = clock();
+    // clock_gettime(CLOCK_MONOTONIC, &before);
     sem_wait(&rw_mutex);
-    printf("writer\n");
+    // clock_gettime(CLOCK_MONOTONIC, &after);
+    clock_t wait = clock() - start_time;
+    double time = ((double)wait) / CLOCKS_PER_SEC;
+    
+    // double seconds = (double) (after.tv_sec - before.tv_sec) * 1000000;
+    // double nanosec = (after.tv_nsec - before.tv_nsec) / 1000;
+    // double time = seconds + nanosec;
+
+
+
+    if (time < min_waitW)
+    {
+      min_waitW = time;
+    }
+
+    if (time > max_waitW)
+    {
+      max_waitW = time;
+    }
+
+    avg_waitW += time;
+
+    printf("writer ");
     sem_post(&rw_mutex);
+    usleep(random);
     i++;
-  } //while (1);
+  }
 }
 
 int main(int argc, char *argv[])
 {
-
-// printf("hellomain");
   int total_readers = 500;
   int total_writers = 10;
   int reader_count = atoi(argv[1]);
   int writer_count = atoi(argv[2]);
 
-  // Initation of semaphores
   sem_init(&rw_mutex, 0, 1);
   sem_init(&mutex, 0, 1);
 
-  // Instantiating the threads for the writters and readers
-  pthread_t *writer_threads = malloc(sizeof(pthread_t) *total_writers);
-  pthread_t *reader_threads= malloc(sizeof(pthread_t) *total_readers);
+  pthread_t *writer_threads = malloc(sizeof(pthread_t) * total_writers);
+  pthread_t *reader_threads = malloc(sizeof(pthread_t) * total_readers);
 
-  printf("Start creation of Writers\n");
-  // Creating the writers threads
+  // printf("Start creation of Writers\n");
+
   for (int i = 0; i < total_writers; i++)
   {
     pthread_create(&writer_threads[i], NULL, writer, &writer_count);
   }
 
-  printf("Start creation of Readers\n");
-  // Creating the readers threads
+  // printf("Start creation of Readers\n");
+
   for (int i = 0; i < total_readers; i++)
   {
     pthread_create(&reader_threads[i], NULL, reader, &reader_count);
   }
 
-  // All the actions is hapenning here
   printf("Wait for Readers\n");
-  // Collect all the readers
+
   for (int i = 0; i < total_readers; i++)
   {
     printf("Waiting for : %d\n", i);
@@ -89,50 +147,20 @@ int main(int argc, char *argv[])
   }
 
   printf("Wait for Writers\n");
-  // Collect all the writers
+
   for (int i = 0; i < total_writers; i++)
   {
     printf("Waiting for : %d\n", i);
     pthread_join(writer_threads[i], NULL);
   }
+
+  avg_waitR = avg_waitR / (total_readers * reader_count);
+  printf("Min read time: %lf\n", min_waitR);
+  printf("Max read time: %lf\n", max_waitR);
+  printf("The average wait time for reader is: %lf\n", avg_waitR);
+  printf("Writer:\n");
+  avg_waitW = avg_waitW / (total_writers * writer_count);
+  printf("Min read time: %lf\n", min_waitW);
+  printf("Max read time: %lf\n", max_waitW);
+  printf("The average wait time for writer is: %lf\n", avg_waitW);
 }
-
-
-// int main()
-// {
-//   int i,b;
-//   pthread_t rtid[5],wtid[5];
-//   sem_init(&mutex,0,1);
-//   sem_init(&rw_mutex,0,1);
-//   for(i=0;i<=2;i++)
-//   {
-//     pthread_create(&wtid[i],NULL,writer,(void *)i);
-//     pthread_create(&rtid[i],NULL,reader,(void *)i);
-//   }
-//   for(i=0;i<=2;i++)
-//   {
-//     pthread_join(wtid[i],NULL);
-//     pthread_join(rtid[i],NULL);
-//   }
-//   return 0;
-// }
-
-// do {
-//     sem_wait(rw_mutex);
-
-// } while (true);
-
-// do {
-//     sem_wait(mutex);
-//     read_count++;
-//     if (read_count == 1) {
-//         sem_wait(rw_mutex);
-//     }
-//     // SIGNAL MUTEX
-
-//     sem_wait(mutex);
-//     read_count--;
-//     if (read_count == 0) {
-//         // SIGNAL RW_MUTEX
-//     }
-// } while (true);
